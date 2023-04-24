2b50cb381c40cf005b50a3abe24dd9a8cb762ecd
Cristi Rusu <CristiInfo@users.noreply.github.com>
2023-04-24 22:22:03 +0300

    Lesson 2

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
-basic.showIcon(IconNames.Heart)
+cuteBot.forward()
 basic.forever(function () {
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(100, 40)
+    if (input.lightLevel() < 10) {
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff0000)
+    } else {
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x007fff)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x007fff)
+    }
     basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(40, 100)
-    basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
 })
